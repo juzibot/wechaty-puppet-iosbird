fcb38f9bf594970f198181e2b15f9e14b455dbd5
linyimin-bupt <linyimin520812@gmail.com>
2019-01-13 00:55:01 +0800

    Add message type example

--- example/starter-bot.ts
+++ example/starter-bot.ts
@@ -1,5 +1,6 @@
 import { PuppetIosbird } from '../src/puppet-iosbird'
-import { Wechaty    } from '../wechaty/src'
+import { Wechaty, Message    } from '../wechaty/src'
+import { MessageType } from '../wechaty-puppet/src';
 
 const puppet  = new PuppetIosbird()
 const bot = new Wechaty({ puppet })
@@ -10,12 +11,55 @@ bot
 .on('message', async (message) => {
   console.log(`Message: ${message.text()}`)
   const content = message.text()
-  if (content === 'test') {
-    message.say('我是机器人')
+
+  const messageType = message.type()
+
+  /**
+   * Message
+   */
+  switch (messageType) {
+    case MessageType.Text: {
+      console.log('#####################################')
+      console.log('message type is text')
+      console.log('#####################################')
+      break
+    }
+    case MessageType.Image: {
+      console.log('#####################################')
+      console.log('message type is image')
+      console.log('#####################################')
+      break
+    }
+
+    case MessageType.Emoticon: {
+      console.log('#####################################')
+      console.log('message type is Emotion')
+      console.log('#####################################')
+      break
+    }
+    case MessageType.Audio: {
+      console.log('#####################################')
+      console.log('message type is Audio')
+      console.log('#####################################')
+      break
+    }
+    default: {
+      console.log('#####################################')
+      console.log('message type is Other')
+      console.log('#####################################')
+      break
+    }
+
   }
 
-  console.log(content)
+  console.log('#####################################')
+  console.log('message content:' + content)
+  console.log('#####################################')
 
+
+  /**
+   * Room
+   */
   const room = message.room()
   if (room) {
     const members = await room.memberAll()
